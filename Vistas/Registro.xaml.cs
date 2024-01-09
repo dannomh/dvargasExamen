ea20730346c78510b60fad837ad12ff550dc4ce7
master <master@SMLAB310PC08_23>
2024-01-08 20:59:58 -0500

    final

--- Vistas/Registro.xaml.cs
+++ Vistas/Registro.xaml.cs
@@ -25,12 +25,41 @@ public partial class Registro : ContentPage
         }
     }
 
-    private void btbacercade_Clicked(object sender, EventArgs e)
+    public void btbacercade_Clicked(object sender, EventArgs e)
     {
         double inicial = Convert.ToDouble(txtMonto.Text);
 
-        double montofinalmensual = (1500 - inicial/4)+(0,4*500);
-        txtPagoInicial.Text = montofinalmensual.ToString();
+
+        double montofinalmensual = (1500 - inicial) / 4 + 0.04 * 1500;
+
+        if(inicial == 1500)
+        {
+            double montocero  = 1500 - inicial;
+            txtPagoInicial.Text = montocero.ToString();
+        }
+        else
+        {
+            txtPagoInicial.Text = montofinalmensual.ToString();
+        }
+       
         
+    }
+
+    private void btnResumen_Clicked(object sender, EventArgs e)
+    {
+        double monto = Convert.ToDouble(txtMonto.Text);
+        double inicial = Convert.ToDouble(txtMonto.Text);
+
+        double montofinalmensual = (1500 - inicial) / 4 + 0.04 * 1500;
+        double pagototal = inicial + montofinalmensual * 4;
+        string nombre = txtNombre.Text;
+        string apellido = txtApellido.Text;
+        string edad = txtEdad.Text;
+        string fecha = dpFechas.Date.ToString();
+        string ciudad = dpciudad.Items[dpciudad.SelectedIndex].ToString();
+        string pais = dppaises.Items[dppaises.SelectedIndex].ToString();
+        DisplayAlert("Alerta", "Nombre: "+nombre.ToString()+"\nApellido: "+ apellido.ToString()+ "\nEdad: "+ edad.ToString()
+            +"\nFecha"+fecha.ToString()+"\nCiudad: "+ciudad.ToString()+"\nPais:"+pais.ToString()+ "\nMonto: "+ monto.ToString()+
+            "\nMensual: "+ montofinalmensual.ToString()+"\nPago Total: "+pagototal.ToString(), "Cancelar");
     }
 }
