4659d91c4d7f7ac50a359907ba6a7c03dbf5709a
master <master@SMLAB306PC05_23>
2024-01-08 19:28:18 -0500

    final

--- Vistas/Registro.xaml.cs
+++ Vistas/Registro.xaml.cs
@@ -10,6 +10,24 @@ public partial class Registro : ContentPage
 
     private void txtMonto_TextChanged(object sender, TextChangedEventArgs e)
     {
-        //
+        try
+        {
+            double monto = Convert.ToDouble(txtMonto.Text);
+            if (monto > 1500 || monto < 0)
+            {
+                DisplayAlert("Alerta", "Rango 0 - 1500", "Cancelar");
+                txtMonto.Text = "";
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+    }
+
+    private void btbacercade_Clicked(object sender, EventArgs e)
+    {
+        double mensual = Convert.ToDouble(txtMonto.Text);
+        
     }
 }
